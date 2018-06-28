70c1de3f2fe863898d72fe9db55fa7cc294a2610
hans511002 <hans511002@sohu.com>
2018-06-28 19:18:11 +0800

    f

---
+++ Classes/engine/level/RoadLevels_mc.cpp
@@ -0,0 +1,33 @@
+﻿ 
+#include "RoadLevels_mc.h"
+
+namespace engine{
+    RoadLevels_mc::RoadLevels_mc(int level)
+    {
+        if(level==1)
+        {
+            
+        } else if(level == 2) {
+        } else if(level == 3 ) {
+        } else if(level == 4 ) {
+        } else if(level == 5 ) {
+        } else if(level == 6 ) {
+        } else if(level == 7 ) {
+        } else if(level == 8 ) {
+        } else if(level == 9 ) {
+        } else if(level == 10) {
+        } else if(level == 11) {
+        } else if(level == 12) {
+        } else if(level == 13) {
+        } else if(level == 14) {
+        } else if(level == 15) { 
+            
+        }
+        
+        init();
+    };
+    bool RoadLevels_mc::init()
+    {
+        BaseNode::init();
+    };
+}
---
+++ Classes/engine/level/RoadLevels_mc.h
@@ -0,0 +1,31 @@
+﻿#ifndef ENGINE_LVEL_ROADS_H
+#define ENGINE_LVEL_ROADS_H
+#include "BaseHeaders.h"
+
+namespace engine{
+	
+	struct Sector
+	{
+	    float x,y;
+	    float w,h;
+	    inline Sector(float x,float y,float w,float h)
+	    {
+	        this->x=x;
+	        this->y=y;
+	        this->w=w;
+	        this->h=h;
+	    }
+	};
+	class RoadLevels_mc : public BaseNode{
+	public:
+		int level;
+		Common::Array<Sector> sectors;
+        Common::Array<BaseNode *> segments;
+        
+		RoadLevels_mc(int level);
+		virtual bool init();
+	};
+ 
+}
+#endif
+
